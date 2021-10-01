95bc912577c9fe1b293e53d0ab110e5d5b3491d9
John Hurdle <johnhurdle23@gmail.com>
2021-09-30 19:38:16 -0700

    after twitter api and sqlalchemy

--- twitter/app.py
+++ twitter/app.py
@@ -1,38 +1,26 @@
-from flask import Flask
-import sqlite3
-
+from flask import Flask, request
+from twitter_functions import upsert_author
+from data_model import DB
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:////Users/johnhurdle/Dropbox/PS/clients/Lambda/repos/dspt13/twitter/twitter_db.sqlite3'
+DB.init_app(app)
 
 
 @app.route('/')
 def landing():
-
-    conn = sqlite3.connect('/Users/johnhurdle/Dropbox/PS/clients/Lambda/repos/dspt13/rpg_db.sqlite3')
-
-    curs = conn.cursor()
-
-    curs.execute("""select * from charactercreator_character limit 10""")
-
-    results = curs.fetchall()
-
-    conn.close()
-
-    return app.send_static_file('landing.html')
+    DB.create_all()
+    return 'welcome to my twitter app'
 
 
-@app.route('/items')
-def items():
-
-    conn = sqlite3.connect('/Users/johnhurdle/Dropbox/PS/clients/Lambda/repos/dspt13/rpg_db.sqlite3')
-
-    curs = conn.cursor()
-
-    curs.execute("""select * from armory_item limit 10""")
-
-    results = curs.fetchall()
-
-    return str(results)
+@app.route('/add_author')
+def add_author():
+    author_handle = request.args['author_handle']
+    author = upsert_author(author_handle=author_handle)
+    return ', '.join([t.body for t in author.tweets])
 
 
 app.run()
---
+++ twitter/twitter_functions.py
@@ -0,0 +1,33 @@
+import tweepy
+from data_model import DB, Tweet, Author
+import numpy as np
+import os
+
+twitter_auth = tweepy.OAuthHandler(os.environ['twitter_key'], os.environ['twitter_secret'])
+twitter_api = tweepy.API(twitter_auth)
+
+
+def upsert_author(author_handle):
+    twitter_author = twitter_api.get_user(user_id=author_handle)
+    if Author.query.get(twitter_author.id):
+        db_author = Author.query.get(twitter_author.id)
+    else:
+        db_author = Author(id=twitter_author.id, name=author_handle)
+        DB.session.add(db_author)
+
+    author_tweet_ids = [tweet.id for tweet in Tweet.query.filter(Tweet.author_id == db_author.id)]
+
+    if len(author_tweet_ids) > 0:
+        last_tweet_stored_id = np.max(author_tweet_ids)
+        author_tweets = twitter_author.timeline(count=200, exclude_replies=True, include_rts=False,
+                                                tweet_mode='extended', since_id=last_tweet_stored_id)
+    else:
+        author_tweets = twitter_author.timeline(count=200, exclude_replies=True, include_rts=False,
+                                                tweet_mode='extended')
+
+    for tweet in author_tweets:
+        db_tweet = Tweet(id=tweet.id, body=tweet.full_text)
+        db_author.tweets.append(db_tweet)
+        DB.session.add(db_tweet)
+    DB.session.commit()
+    return db_author
